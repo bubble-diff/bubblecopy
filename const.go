c54d84811ed14aa71a8e523043e05b50a9a1032f
peanutzhen <astzls213@gmail.com>
2022-03-14 12:23:12 +0800

    优化代码

--- const.go
+++ const.go
@@ -2,8 +2,8 @@ package main
 
 // 参数常量
 const (
-	SnapshotLen         int32 = 65536 * 4
-	SettingsFilePath          = "./settings.json"
+	SnapshotLen      int32 = 65536 * 4
+	SettingsFilePath       = "./settings.json"
 )
 
 // 应用层协议类型
